259ba35882b8dda0f40512b8d9dacce7b4da2945
nodes777 <nodes777@gmail.com>
2018-03-10 22:52:19 -0800

    inits chart

--- js/main.js
+++ js/main.js
@@ -1,2 +1,55 @@
-console.log(`Main has d3: ${d3}`);
+var data = exampleData;
+// set the dimensions and margins of the graph
+var margin = {top: 20, right: 20, bottom: 30, left: 40},
+    width = 960 - margin.left - margin.right,
+    height = 60*exampleData.length - margin.top - margin.bottom;
+
+// set the ranges
+var y = d3.scaleBand()
+          .range([height, 0])
+          .padding(0.1);
+
+var x = d3.scaleLinear()
+          .range([0, width]);
+
+// append the svg object to the body of the page
+// append a 'group' element to 'svg'
+// moves the 'group' element to the top left margin
+var svg = d3.select("#chart").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform",
+          "translate(" + margin.left + "," + margin.top + ")");
+
+  // format the data
+  data.forEach(function(d) {
+    d.count = +d.count;
+  });
+
+  data.sort(function(a, b){return a.count - b.count});
+
+  // Scale the range of the data in the domains
+  x.domain([0, d3.max(data, function(d){ return d.count; })])
+  y.domain(data.map(function(d) { return d.name; }));
+  //y.domain([0, d3.max(data, function(d) { return d.count; })]);
+
+  // append the rectangles for the bar chart
+  svg.selectAll(".bar")
+      .data(data)
+    .enter().append("rect")
+      .attr("class", "bar")
+      //.attr("x", function(d) { return x(d.count); })
+      .attr("width", function(d) {return x(d.count); } )
+      .attr("y", function(d) { return y(d.name); })
+      .attr("height", y.bandwidth());
+
+  // add the x Axis
+  svg.append("g")
+      .attr("transform", "translate(0," + height + ")")
+      .call(d3.axisBottom(x));
+
+  // add the y Axis
+  svg.append("g")
+      .call(d3.axisLeft(y));
 
